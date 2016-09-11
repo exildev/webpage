0e94eb6f781b0920edbde999499a718b5555ffcd
luismoralesp <luismiguel.mopa@gmail.com>
2016-09-11 14:23:53 -0500

    slide

---
+++ slide.js
@@ -0,0 +1,22 @@
+(function() {
+		
+	function load(){
+		var parents = document.querySelectorAll(".parent");
+		var i = 0;
+		window.slide_down = function (){
+   			if (parents[i]){
+		   		parents[i].classList.add("open");
+		   		i++;
+	   		}
+	   	};
+		window.slide_up = function (){
+   			if (i > 0){
+		   		i--;
+		   		parents[i].classList.remove("open");
+	   		}
+	   	};
+   	}
+
+
+	document.addEventListener("DOMContentLoaded", load, false);
+})();
