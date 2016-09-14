217d5b6ace8a03da2b5ce12da669f2a7c60f02e0
mariobp <mariobarrpach@gmail.com>
2016-09-14 18:27:56 -0500

    paginas

--- exile/views.py
+++ exile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 import models
 # Create your views here.
 
@@ -10,4 +11,13 @@ def index(request):
         items = models.OrdenItem.objects.filter(menu=principal.menu).first()
     # end if
     return render(request, 'exile/index.html', {'items': items})
+# end def
+
+
+def page(request, id):
+    pagina = models.Page.objects.filter(id=id).first()
+    if pagina:
+        return render(request, 'exile/page.html', {'contenido': pagina})
+    # end if
+    raise Http404("Poll does not exist")
 # end def
