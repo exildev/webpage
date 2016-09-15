07d1235279a48d44d0577cd5222b0e2fbb7f4622
darkdrei <darkdrei88@gmail.com>
2016-09-15 17:17:26 -0500

    actualizacion animacion

--- exile/static/exile/resources/js/servi_scale.js
+++ exile/static/exile/resources/js/servi_scale.js
@@ -1,152 +1,107 @@
 (function() {
     function funciones() {
-        console.log("1");
-        window.contentDelay = 400;
-        window.isOpen = false;
-        window.w = window;
-        window.item=document.getElementsByClassName('item');
-        window.close = function(event) {
-
-            event.preventDefault();
-            event.stopImmediatePropagation();
-
-            var target = event.target;
-            var div = document.getElementById('modal__temp');
-
-            /**
-             * make sure the modal__bg or modal__close was clicked, we don't want to be able to click
-             * inside the modal and have it close.
-             */
-
-            if (isOpen && target.classList.contains('modal__bg') || target.classList.contains('modal__close')) {
-                // make the hidden div visible again and remove the transforms so it scales back to its original size
-                console.log(div);
-                div.style.opacity = '1';
-                div.removeAttribute('style');
-                /**
-                 * iterate through the modals and modal contents and triggers to remove their active classes.
-                 * remove the inline css from the trigger to move it back into its original position.
-                 */
-                 div.style.visibility = 'hidden';
-                 div.addEventListener('transitionend', removeDiv, false);
-                 document.getElementById('modal').classList.remove('modal--active');
-                 var contenidos = document.getElementsByClassName("modal__content");
-                 for (var i = 0; i < contenidos.length; i++) {
-                     contenidos[i].classList.add("desaparecer");
-                     contenidos[i].classList.remove('modal__content--active');
-                 }
-                var item = document.getElementsByClassName('item');
-                for (var i = 0; i < item.length; i++) {
-                    //modals[i].classList.remove('modal--active');
-                    //content[i].classList.remove('modal__content--active');
-                    item[i].style.transform = 'none';
-                    item[i].style.webkitTransform = 'none';
-                    item[i].classList.remove('modal__trigger--active');
+        window.animando=false;
+        window.item =null;
+        window.coordenadas=null;
+        window.scalar = function(event){
+            if(window.animando){
+                window.animando=false;
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                console.log("termino la animacion");
+                var css = "z-index: 10;position: relative;-ms-transform: translate(50px,100px);/* IE 9 */";
+                    css+="-webkit-transform: translate("+(-1*window.coordenadas.mover_x)+"px,"+(-1*window.coordenadas.mover_y)+"px);/*Safari */";
+                    css+="transform: translate("+(-1*window.coordenadas.mover_x)+"px,"+(-1*window.coordenadas.mover_y)+"px); /* Standard syntax */";
+                    css+="-webkit-transition-property: transform ; /* Safari */";
+                    css+="-webkit-transition-duration: 5s; /* Safari */";
+                    css+="-webkit-transition-delay: 2s; /* Safari */";
+                    css+="transition-property: transform;"
+                    css+="transition-duration: 5s;"
+                    css+="transition-delay: 2s;"
+                    css+="-webkit-transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
+                    css+="transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
+                    css+="background-color: lightblue;";
+                    css+="width:"+window.coordenadas.w+"px";
+                    //css+="height:"+window.coordenadas.h+"px";
+                    console.log("esto es el tamaño de y "+window.coordenadas.h);
+                window.item.style.cssText= css;
+                window.item.className ="modal";
+            }
+        };
+        window.mover = function(event){
+            if(!window.animando){
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                var abuelos = document.getElementsByClassName("servi");
+                var altura = 0,
+                    __y=0,
+                    __x =0,
+                    total_h=0;
+                for(var i=0;i < abuelos.length;i++){
+                    if(i===0){
+                        altura = abuelos[i].offsetTop;
+                        __x = abuelos[i].getBoundingClientRect().width;
+                    }
+                    __y = abuelos[i].getBoundingClientRect().height;
                 }
+                /* posicion central de la seccion*/
+                __x=__x/2;
+                total_h=__y;
+                console.log("este es la altuda del contenedor "+total_h);
+                __y=altura+ __y/2;
 
-                // when the temporary div is opacity:1 again, we want to remove it from the dom
-
-
-                isOpen = false;
-
+                console.log(altura,__y,__x,"jajaja");
+                /*coordenadas del elemento*/
+                var x_prima= this.offsetLeft,
+                    y_prima=this.offsetTop,
+                    x_prima_w=this.getBoundingClientRect().width,
+                    x_prima_h=this.getBoundingClientRect().height;
+                    console.log("este es el valor para y : "+x_prima_h);
+                    var y_retorno = x_prima_h;
+                var mover_x=0,
+                    mover_y=0;
+                if(__x<x_prima){
+                    mover_x = -x_prima + __x-150;
+                    console.log("valor x "+mover_x);
+                }else if(__x==x_prima){
+                    mover_x = -150;
+                }else{
+                    mover_x = -x_prima + __x-150;
+                }
+                if(__y>y_prima){
+                    console.log("entro en esta");
+                    mover_y =  __y-y_prima;
+                }else if(y_prima >= __y){
+                    mover_y = __y-y_prima;
+                }
+                console.log("valores para y ( ",__y,y_prima," )");
+                var abuelo = this.parentNode.parentNode;
+                var css = "z-index: 10;position: relative;-ms-transform: translate("+mover_x+"px,"+mover_y+"px);/* IE 9 */";
+                    css+="-webkit-transform: translate("+mover_x+"px,"+mover_y+"px) scaley(5);/*Safari */";
+                    css+="transform: translate("+mover_x+"px,"+mover_y+"px)  scaley(5); /* Standard syntax */";
+                    css+="-webkit-transition-property: transform ; /* Safari */";
+                    css+="-webkit-transition-duration: 5s; /* Safari */";
+                    css+="-webkit-transition-delay: 2s; /* Safari */";
+                    css+="transition-property: transform;"
+                    css+="transition-duration: 5s;"
+                    css+="transition-delay: 2s;"
+                    css+="-webkit-transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
+                    css+="transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
+                    css+="background-color: lightblue;";
+                    css+="width:300px";
+                window.item =this;
+                window.animando=true;
+                window.coordenadas = {x:mover_x,y:mover_y,w:this.getBoundingClientRect().width,h:y_retorno};
+                this.style.cssText= css;
+                this.className = "mod_temp";
             }
-
-            function removeDiv() {
-                //alert("jajaja");
-                setTimeout(function() {
-                    window.requestAnimationFrame(function() {
-                        // remove the temp div from the dom with a slight delay so the animation looks good
-                        div.remove();
-                    });
-                }, window.contentDelay - 50);
-            }
-
         };
-        window.open = function(m, div) {
-
-            if (!isOpen) {
-                // select the content inside the modal
-                var content = m.querySelector('.modal__content');
-                // reveal the modal
-                m.classList.add('modal--active');
-                // reveal the modal content
-                content.classList.add('modal__content--active');
-                content.addEventListener('transitionend', hideDiv, false);
-
-                isOpen = true;
-            }
-
-            function hideDiv() {
-                div.style.opacity = '0';
-                content.removeEventListener('transitionend', hideDiv, false);
-            }
-        };
-        window.moverDiv = function(trig, modal, div) {
-            var trigProps = trig.getBoundingClientRect();
-            var m = modal;
-            var mProps = m.querySelector('.modal__content').getBoundingClientRect();
-            var transX, transY, scaleX, scaleY;
-            var xc = window.w.innerWidth / 2;
-            var yc = window.w.innerHeight / 2;
-
-            // this class increases z-index value so the button goes overtop the other buttons
-            trig.classList.add('modal__trigger--active');
-
-            // these values are used for scale the temporary div to the same size as the modal
-            scaleX = mProps.width / trigProps.width;
-            scaleY = mProps.height / trigProps.height;
-
-            scaleX = scaleX.toFixed(3); // round to 3 decimal places
-            scaleY = scaleY.toFixed(3);
-
-
-            // these values are used to move the button to the center of the window
-            transX = Math.round(xc - trigProps.left - trigProps.width / 2);
-            transY = Math.round(yc - trigProps.top - trigProps.height / 2);
-
-            // if the modal is aligned to the top then move the button to the center-y of the modal instead of the window
-            if (m.classList.contains('modal--align-top')) {
-                transY = Math.round(mProps.height / 2 + mProps.top - trigProps.top - trigProps.height / 2);
-            }
-            // translate button to center of screen
-            trig.style.transform = 'translate(' + transX + 'px, ' + transY + 'px)';
-            trig.style.webkitTransform = 'translate(' + transX + 'px, ' + transY + 'px)';
-            // expand temporary div to the same size as the modal
-            div.style.transform = 'scale(' + scaleX + ',' + scaleY + ')';
-            div.style.webkitTransform = 'scale(' + scaleX + ',' + scaleY + ')';
-
-            window.setTimeout(function() {
-                window.requestAnimationFrame(function() {
-                    open(m, div);
-                });
-            }, window.contentDelay);
+        var item = document.getElementsByClassName("modal");
+        for(var i=0;i < item.length; i++){
+            item[i].addEventListener("click",mover,false);
+            item[i].addEventListener("click",scalar,false);
         }
-        window.crearDiv = function(self, modal) {
-            var tem_div = document.getElementById('tem_mod');
-            if (tem_div === null) {
-                var div = document.createElement('div');
-                div.id = 'modal__temp';
-                self.appendChild(div);
-                moverDiv(self, modal, div);
-            }
-        };
-        window.getIdentificador = function(event) {
-            console.log("llego");
-            event.preventDefault();
-            var modal = document.getElementById('modal');
-            crearDiv(this, modal);
-        };
-        console.log("1");
-        var item = window.item;
-        console.log(item.length);
-        for (var i = 0; i < item.length; i++) {
-            item[i].addEventListener('click', getIdentificador, false);
-        }
-        var mod = document.getElementsByClassName("modal_bg");
-
-        for (var i = 0; i < mod.length; i++) {
-            mod[i].addEventListener('click', close, false);
-        }
+        //this.addEventListener('transitionend', scalar, false);
     }
     document.addEventListener("DOMContentLoaded", funciones, false);
 })();
