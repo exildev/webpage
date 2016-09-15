a579ec4b3b73dd04cbc14d51f56889e8e635b42e
mariobp <mariobarrpach@gmail.com>
2016-09-15 15:38:24 -0500

    css

--- exile/static/exile/js/contacto.js
+++ exile/static/exile/js/contacto.js
@@ -1,10 +1,8 @@
 var formContainer = $('#form-container');
 
-
 bindFormClick();
 //Opening the form
 function bindFormClick(){
-  console.log("contacto form");
   $(formContainer).on('click', function(e) {
     e.preventDefault();
     toggleForm();
@@ -14,7 +12,7 @@ function bindFormClick(){
 }
 
 //Closing the form
-$('#form-close, .form-overlay').click(function(e) {
+$('#form-close').click(function(e) {
   e.stopPropagation();
   e.preventDefault();
   toggleForm();
@@ -24,7 +22,7 @@ $('#form-close, .form-overlay').click(function(e) {
 function toggleForm(){
   $(formContainer).toggleClass('expand');
   $(formContainer).children().toggleClass('expand');
-  $('.contacto').toggleClass('show-form-overlay');
+  $('.form-contacto').toggleClass('show-form-overlay');
   $('.form-submitted').removeClass('form-submitted');
 }
 
@@ -50,7 +48,7 @@ $('form').submit(function() {
   });
 
   if (!formError) {
-    $('.contacto').addClass('form-submitted');
+    $('.form-contacto').addClass('form-submitted');
     $('#form-head').addClass('form-submitted');
     setTimeout(function(){
       $(form).trigger("reset");
@@ -62,4 +60,4 @@ $('form').submit(function() {
 function isValidEmail(email) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(email);
-};
+}
