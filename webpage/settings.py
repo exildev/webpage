2fcc7aed248684481450109867cad37f70884516
mariobp <mariobarrpach@gmail.com>
2016-09-15 11:27:57 -0500

    settings

--- webpage/settings.py
+++ webpage/settings.py
@@ -56,17 +56,17 @@ SUIT_CONFIG = {
     # menu
     # 'SEARCH_URL': '/admin/auth/user/',
      'MENU_ICONS': {
-        'sites': 'icon-leaf',
         'auth': 'icon-lock',
+        'exile': 'icon-fire'
      },
     # 'MENU_OPEN_FIRST_CHILD': True, # Default True
     # 'MENU_EXCLUDE': ('auth.group',),
-    # 'MENU': (
-    #     'sites',
-    #     {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
-    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
-    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
-    # ),
+    'MENU': (
+         'sites',
+         {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
+         {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
+         {'app': 'exile', 'models': ('page', 'item', 'itemseccion', 'subitem', 'menu', 'menuprincipal', 'paginaprincipal')}
+    ),
 
     # misc
     'LIST_PER_PAGE': 15
