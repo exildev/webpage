3eaeefd71d5363070a6dad042195a280cac42a9b
mariobp <mariobarrpach@gmail.com>
2016-09-15 01:45:39 -0500

    navegacion lista

--- exile/templatetags/menu_filter.py
+++ exile/templatetags/menu_filter.py
@@ -9,15 +9,39 @@ def ordenItem(menu):
 # end def
 
 
+@register.filter(name="subitems_tag")
+def ordenSubItem(item, seccion):
+    return item.subitems.all().filter(ordensubitem__seccion=seccion).order_by('ordensubitem__posicion')
+# end def
+
+
 @register.filter(name="contenido_tag")
 def ordernContenido(page):
     secciones = models.Seccion.objects.filter(pagina=page).order_by('posicion')
-    print secciones
     return secciones
 # end def
 
 
-def subItemsSeccion(subitems):
-    secciones = models.ItemSeccion.objects.all()
-    return
+@register.filter(name="subseccion_tag")
+def subItemsSeccion(item):
+    secciones = []
+    for s in item.subitems.all():
+        orden = models.OrdenSubItem.objects.filter(item=item, subitem=s).first()
+        if orden.seccion:
+            if secciones.count(orden.seccion.nombre) == 0:
+                secciones.append(orden.seccion)
+            # end if
+    # end if
+    return secciones
+# end def
+
+
+@register.filter(name="col_num")
+def colNum(total, col):
+    if total == 0:
+        num = col
+    else:
+        num = col/total
+    # end if
+    return col
 # end def
