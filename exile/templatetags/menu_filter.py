059617d6f8f9b1e57ee7cad6085e74c2b5431a00
mariobp <mariobarrpach@gmail.com>
2016-09-15 10:47:35 -0500

    tag

--- exile/templatetags/menu_filter.py
+++ exile/templatetags/menu_filter.py
@@ -11,6 +11,7 @@ def ordenItem(menu):
 
 @register.filter(name="subitems_tag")
 def ordenSubItem(item, seccion):
+    print seccion
     if seccion:
         return item.subitems.all().filter(ordensubitem__seccion=seccion).order_by('ordensubitem__posicion')
     # end if
