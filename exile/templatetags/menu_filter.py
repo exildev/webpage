43dbbe1cc1eb466f0199d66a4066025b6b4f5ac8
mariobp <mariobarrpach@gmail.com>
2016-09-16 16:57:42 -0500

    footer admin

--- exile/templatetags/menu_filter.py
+++ exile/templatetags/menu_filter.py
@@ -43,7 +43,7 @@ def subItemsSeccion(item):
 def footerSeccion(footer):
     secciones = []
     for f in footer.paginas.all():
-        orden = models.OrdenFooter.objects.filter(footer=footer, pagina=f)
+        orden = models.OrdenFooter.objects.filter(footer=footer, pagina=f).first()
         if secciones.count(orden.seccion) == 0:
             secciones.append(orden.seccion)
         # end if
