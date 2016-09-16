23fdb14c4ba6cfad29fa7b4bcd8f68820c62f81f
mariobp <mariobarrpach@gmail.com>
2016-09-15 23:45:57 -0500

    Admin contacto

--- exile/urls.py
+++ exile/urls.py
@@ -4,5 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^page/(?P<id>\d+)/$', views.page, name="page"),
-    ull(r'^contacto/', views.ContactoSupra.as_view(), name="contacto" )
+    url(r'^contacto/', views.ContactoSupra.as_view(), name="contacto" )
 ]
