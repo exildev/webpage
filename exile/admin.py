75a3af28cb80709cf911d046f126fd3265009bf8
mariobp <mariobarrpach@gmail.com>
2016-09-15 23:46:29 -0500

    Admin contacto

--- exile/admin.py
+++ exile/admin.py
@@ -82,7 +82,7 @@ class PaginaPrincipalAdmin(admin.ModelAdmin):
 
 
 class ContactoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'emal', 'asunto', 'mensaje')
+    list_display = ('nombre', 'email', 'asunto', 'mensaje')
     search_fields = list_display
 # end class
 
