1f5f4f6ccfda1845c8b21960ec54b070718de3e0
darkdrei <darkdrei88@gmail.com>
2016-09-19 17:18:05 -0500

    actualizacion

--- exile/static/exile/resources/js/servi_scale.js
+++ exile/static/exile/resources/js/servi_scale.js
@@ -17,7 +17,7 @@
                 if (texto != undefined){
                     texto[0].className="des_complem";
                 }
-                console.log("termino la animacion");
+                //console.log("termino la animacion");
                 var css = "z-index: 0;position: relative;-ms-transform: translate(50px,100px);/* IE 9 */";
                     css+="-webkit-transform: translate("+(-1*window.coordenadas.mover_x)+"px,"+(-1*window.coordenadas.mover_y)+"px);/*Safari */";
                     css+="transform: translate("+(-1*window.coordenadas.mover_x)+"px,"+(-1*window.coordenadas.mover_y)+"px); /* Standard syntax */";
@@ -52,39 +52,43 @@
                     __y=0,
                     __x =0,
                     total_h=0;
+                var xf=0,yf=0;
                 for(var i=0;i < abuelos.length;i++){
                     if(i===0){
                         altura = abuelos[i].offsetTop;
                         __x = abuelos[i].getBoundingClientRect().width;
+                        xf = abuelos[i].getBoundingClientRect().width*0.8;
+                        yf = abuelos[i].getBoundingClientRect().height*2;
                     }
                     __y = abuelos[i].getBoundingClientRect().height;
                 }
+                console.log(" los tamaños a escalar "+xf+"  "+yf);
                 /* posicion central de la seccion*/
                 __x=__x/2;
                 total_h=__y;
-                console.log("este es la altuda del contenedor "+total_h);
+                //console.log("este es la altuda del contenedor "+total_h);
                 __y=altura+ __y/2;
 
-                console.log(altura,__y,__x,"jajaja");
+                //console.log(altura,__y,__x,"jajaja");
                 /*coordenadas del elemento*/
                 var x_prima= this.offsetLeft,
                     y_prima=this.offsetTop,
                     x_prima_w=this.getBoundingClientRect().width,
                     x_prima_h=this.getBoundingClientRect().height;
-                    console.log("este es el valor para y : "+x_prima_h);
+                    //console.log("este es el valor para y : "+x_prima_h);
                     var y_retorno = x_prima_h;
                 var mover_x=0,
                     mover_y=0;
                 if(__x<x_prima){
                     mover_x = -x_prima + __x-150;
-                    console.log("valor x "+mover_x);
+                    //console.log("valor x "+mover_x);
                 }else if(__x==x_prima){
                     mover_x = -150;
                 }else{
                     mover_x = -x_prima + __x-150;
                 }
                 if(__y>y_prima){
-                    console.log("entro en esta");
+                    //console.log("entro en esta");
                     mover_y =  __y-y_prima;
                 }else if(y_prima >= __y){
                     mover_y = __y-y_prima;
@@ -102,21 +106,22 @@
                 if (texto != undefined && window.animando){
                     texto[0].className="act_complem";
                 }
-                console.log("valores para y ( ",__y,y_prima," )");
+                //console.log("valores para y ( ",__y,y_prima," )");
                 var abuelo = this.parentNode.parentNode;
-                var css = "z-index: 10;position: relative;-ms-transform: translate("+mover_x+"px,"+mover_y+"px);/* IE 9 */";
-                    css+="-webkit-transform: translate("+mover_x+"px,"+mover_y+"px) scaley(2) scalex(3);/*Safari */";
-                    css+="transform: translate("+mover_x+"px,"+mover_y+"px)  scaley(2) scalex(3); /* Standard syntax */";
-                    css+="-webkit-transition-property: transform ; /* Safari */";
-                    css+="-webkit-transition-duration: 5s; /* Safari */";
-                    css+="-webkit-transition-delay: 2s; /* Safari */";
-                    css+="transition-property: transform;"
-                    css+="transition-duration: 5s;"
-                    css+="transition-delay: 2s;"
+                var css = "z-index: 10;position: relative;-ms-transform: translate("+mover_x+"px,"+(mover_y)+"px);/* IE 9 */";
+                    css+="-webkit-transform: translate("+mover_x+"px,"+mover_y+"px);";
+                    css+="transform: translate("+(mover_x+(-100))+"px,"+mover_y+"px)  ;";
+                    css+="-webkit-transition-property: transform,width  ; /* Safari */";
+                    css+="-webkit-transition-duration: 5s,2s; /* Safari */";
+                    css+="-webkit-transition-delay: 2s,2s; /* Safari */";
+                    css+="transition-property: transform,width ;";
+                    css+="transition-duration: 5s,2s;";
+                    css+="transition-delay: 2s,2s;";
+                    css+="border-radius: 3px;";
+                    css+="box-shadow: 0 0 12 #555;";
                     css+="-webkit-transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
                     css+="transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);";
-                    css+="background-color: white;";
-                    css+="width:300px";
+                    css+="width:500px;";
                 window.item =this;
 
                 window.coordenadas = {x:mover_x,y:mover_y,w:this.getBoundingClientRect().width,h:y_retorno};
